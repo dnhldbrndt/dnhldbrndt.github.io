63ca35188e10fe3e90c57d0aabb7242cfa0121c2
dnhldbrndt <dnlhldbrndt@gmail.com>
2024-08-14 06:58:03 +0200

    initial commit

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Texture loader
+const loader = new THREE.TextureLoader()
+const cross = loader.load('./cross.png')
+
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,16 +18,41 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.CircleGeometry(20);
+
+const particlesGeometry = new THREE.BufferGeometry;
+const particlesCnt = 5000;
+
+const posArray = new Float32Array(particlesCnt * 3);
+// xyz, xyz, xyz
+
+for(let i = 0; i < particlesCnt * 3; i++) {
+   // posArray[i] = Math.random();
+    // posArray[i] = Math.random() - 0.5;
+    // posArray[i] = (Math.random() - 0.5) * 5;
+    posArray[i] = (Math.random() - 0.5) * (Math.random() * 5);
+}
+gui.hide();
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3)); 
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.PointsMaterial({
+    size: 0.005,
+ 
+})
+
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.005,
+    map: cross,
+    transparent: true,
+    blending: THREE.AdditiveBledning
+})
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const sphere = new THREE.Points(geometry,material)
+const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(sphere, particlesMesh)
 
 // Lights
 
@@ -77,6 +107,19 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(new THREE.Color('#0d0f0f'), 1)
+
+//Mouse
+
+document.addEventListener('mousemove', animateParticles)
+
+let mouseX = 0;
+let mouseY = 0;
+
+function animateParticles(event) {
+    mouseY = event.clientY;
+    mouseX = event.clientX;
+}
 
 /**
  * Animate
@@ -91,6 +134,11 @@ const tick = () =>
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+    particlesMesh.rotation.y = -.1 * elapsedTime
+    if (mouseX > 0) { 
+        particlesMesh.rotation.x = -mouseY * (elapsedTime * 0.000008)
+        particlesMesh.rotation.y = -mouseX * (elapsedTime * 0.000008)
+    }
 
     // Update Orbital Controls
     // controls.update()
